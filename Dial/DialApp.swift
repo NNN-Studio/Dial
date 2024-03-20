700048be912a04ccb6479b7310e1abafff440249
KrLite <68179735+KrLite@users.noreply.github.com>
2024-03-20 21:51:20 +0800

    Initial Commit

---
+++ Dial/DialApp.swift
@@ -0,0 +1,17 @@
+//
+//  DialApp.swift
+//  Dial
+//
+//  Created by KrLite on 2024/3/20.
+//
+
+import SwiftUI
+
+@main
+struct DialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
