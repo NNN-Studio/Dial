1e06ed8b7ea4f9339eafb5168b64e4ac3195d466
KrLite <68179735+KrLite@users.noreply.github.com>
2024-03-30 13:02:16 +0800

    icon chooser

--- Dial/Settings/IconChooserView.swift
+++ Dial/Settings/IconChooserView.swift
@@ -10,27 +10,14 @@ import SFSafeSymbols
 
 struct IconChooserView: View {
     @Binding var chosen: SFSymbol
-    @State var hovering: SFSymbol?
     
-    let columns: [GridItem] = .init(repeating: .init(.fixed(50)), count: 5)
+    let columns: [GridItem] = [.init(.adaptive(minimum: 50, maximum: 100))]
     
     var body: some View {
         ScrollView(.vertical) {
             LazyVGrid(columns: columns) {
                 ForEach(SFSymbol.__circleFillableSymbols) { symbol in
-                    Button {
-                        chosen = symbol
-                    } label: {
-                        Image(systemSymbol: symbol)
-                            .frame(height: 45)
-                            .frame(maxWidth: .infinity)
-                    }
-                    .onHover { isHovering in
-                        hovering = isHovering ? symbol : nil
-                    }
-                    .buttonStyle(.borderless)
-                    .tint(chosen == symbol ? .accentColor : .secondary)
-                    .background(.separator.opacity(symbol == hovering ? 1 : 0), in: .circle)
+                    IconCellView(chosen: $chosen, symbol: symbol)
                 }
             }
             .padding()
@@ -39,6 +26,42 @@ struct IconChooserView: View {
 }
 
 #Preview {
-    IconChooserView(chosen: .constant(.__circleFillableFallback))
-        .frame(height: 420)
+    struct Wrapper: View {
+        @State var chosen: SFSymbol = .__circleFillableFallback
+        
+        var body: some View {
+            IconChooserView(chosen: $chosen)
+                .frame(width: 300, height: 420)
+        }
+    }
+    
+    return Wrapper()
+}
+
+struct IconCellView: View {
+    @Binding var chosen: SFSymbol
+    var symbol: SFSymbol
+    
+    // For animations
+    @State var count: Int = 0
+    
+    var body: some View {
+        Button {
+            count += 1
+            withAnimation(.interactiveSpring) {
+                chosen = symbol
+            }
+        } label: {
+            Image(systemSymbol: symbol)
+                .frame(maxWidth: .infinity)
+                .frame(height: 45)
+                .symbolEffect(.bounce, value: count)
+        }
+        .buttonStyle(.borderless)
+        .tint(chosen == symbol ? .accentColor : .secondary)
+        .background {
+            chosen == symbol ? Color.accentColor.opacity(0.1) : Color.secondary.opacity(0.05)
+        }
+        .clipShape(.capsule)
+    }
 }
